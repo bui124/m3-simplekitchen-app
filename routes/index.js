af72ef1b6fd1f666fd9ecf588d3f339dc82fe282
Neil Bui <neilbui@westcliff.edu>
2021-04-10 16:11:55 -0700

    week 6 day 3

---
+++ routes/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const {check, validationResult} = require('express-validator');
+const path = require('path');
+const auth = require('http-auth');
+const bcrypt = require('bcrypt');
+
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
+
+router.get('/', (req, res) => {
+  res.render('form', { title: 'Welcome' });
+});
+
+router.get('/', (req, res) => {
+  res.render('register', { title: 'Registration form' });
+});
+
+router.get('/registrations', (req, res) => {
+  res.render('registrants', { title: 'Registration form' });
+});
+
+router.get('/registrations', basic.check((req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('registrants', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { 
+      res.send('Sorry! Something went wrong.'); 
+    });
+}));
+
+
+router.post('/', 
+    [
+        check('name')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a name'),
+        check('email')
+        .isLength({ min: 1 })
+        .withMessage('Please enter an email'),
+    ],
+    async (req, res) => {
+        //console.log(req.body);
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+          const registration = new Registration(req.body);
+          // generate salt to hash password
+          const salt = await bcrypt.genSalt(10);
+          // set user password to hasghed password
+          registration.password = await bcrypt.hash(registration.password, salt);
+          registration.save()
+            .then(() => {res.send('Thank you for your registration!');})
+            .catch((err) => {
+              console.log(err);
+              res.send('Sorry! Something went wrong.');
+            });
+          } else {
+            res.render('form', { 
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+             });
+          }
+    });
+
+module.exports = router;
